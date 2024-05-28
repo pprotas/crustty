a1929026fb24e34befea25a249e4fd5383ca45e6
Pawel Protas <contact@pawelprotas.com>
2024-05-28 18:07:40 +0200

    refactor: rewrite in glium

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
+use glium::{
+    glutin::{event_loop::EventLoop, window::WindowBuilder, ContextBuilder},
+    Display, *,
+};
 use glutin::{
     event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-    Api, ContextBuilder, GlRequest,
+    event_loop::ControlFlow,
 };
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     std::env::set_var("LIBGL_ALWAYS_SOFTWARE", "1");
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().with_title("CrusTTY");
 
-    let gl_context = ContextBuilder::new()
-        .with_gl(GlRequest::Specific(Api::OpenGl, (3, 3)))
-        .build_windowed(window, &event_loop)
-        .expect("Cannot create windowed context");
+    let context = ContextBuilder::new().with_vsync(true);
 
-    let gl_context = unsafe {
-        gl_context
-            .make_current()
-            .expect("Failed to make context current")
-    };
-
-    gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+    let display = Display::new(window, context, &event_loop)?;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -31,15 +25,12 @@ fn main() {
             Event::LoopDestroyed => (),
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Resized(physical_size) => gl_context.resize(physical_size),
                 _ => (),
             },
             Event::RedrawRequested(_) => {
-                unsafe {
-                    gl::ClearColor(0.0, 0.0, 1.0, 1.0);
-                    gl::Clear(gl::COLOR_BUFFER_BIT);
-                }
-                gl_context.swap_buffers().unwrap();
+                let mut target = display.draw();
+                target.clear_color(0.0, 0.0, 1.0, 1.0);
+                target.finish().unwrap();
             }
             _ => (),
         }
